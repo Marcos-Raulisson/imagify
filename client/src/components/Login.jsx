83298fd3d153688e5808de9f0463a60dbba54ab3
Marcos Raulisson <marcosraulisson@gmail.com>
2024-11-24 18:40:25 -0300

    Adicionando a animação em todos os componentes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,22 +1,29 @@
 import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
+import { motion } from "framer-motion";
 
 const Login = () => {
   const [state, setState] = useState("Login");
-  const {setShowLogin} = useContext(AppContext)
+  const { setShowLogin } = useContext(AppContext);
 
-  useEffect(()=>{
-    document.body.style.overflow = 'hidden';
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
 
-    return ()=>{
-      document.body.style.overflow = 'unset';
-    }
-  },[])
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
 
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
-      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+    <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
+      <motion.form
+        initial={{ opacity: 0.2, y: 50 }}
+        transition={{ duration: 0.3 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
         <h1 className="text-center text-2xl text-neutral-700 font-medium">
           {state}
         </h1>
@@ -63,7 +70,6 @@ const Login = () => {
               className="text-blue-600 cursor-pointer"
               onClick={() => setState("Sign Up")}
             >
-              
               Sign Up
             </span>
           </p>
@@ -74,18 +80,17 @@ const Login = () => {
               className="text-blue-600 cursor-pointer"
               onClick={() => setState("Login")}
             >
-              
               Login
             </span>
           </p>
         )}
         <img
-          onClick={()=>setShowLogin(false)}
+          onClick={() => setShowLogin(false)}
           src={assets.cross_icon}
           alt=""
           className="absolute top-5 right-5 cursor-pointer"
         />
-      </form>
+      </motion.form>
     </div>
   );
 };
